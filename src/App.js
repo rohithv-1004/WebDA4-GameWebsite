f3ea1b159d58e81071c18e89ab650d2b1f7b13d8
rohithv-1004 <145743627+rohithv-1004@users.noreply.github.com>
2024-04-23 10:27:49 +0530

    h1

--- src/App.js
+++ src/App.js
@@ -1,39 +1,13 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const App = () => {
-  const htmlContent = `
-  <!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Game-Time</title>
-  <link rel="stylesheet" href="../src/index.css">
-</head>
-<body>
-  <div class="grid-for-alignment">
-  <div class="left-side-design"><p></p></div>
-  <div class="middle-stuff">
-    <div class="text-align">
-    <h1 class="neon-text">Game Time</h1>
-  </div>
-    <div class="div-for-button">
-      <a href="../src/tetris.html"><button class="buttons" id="tetris">Tetris</button></a>
-      <a href="../src/wordle.html"><button class="buttons" id="wordle">Wordle</button></a> 
-      <a href="../src/snake.html"><button class="buttons" id="snake">Snake</button></a> 
-      <a href="../src/rps.html"><button class="buttons" id="rps">Rock paper scissors</button></a>
-      
-    </div>
-  </div>
-  <div class="right-side-design"><p></p></div>
-  </div>
-</body>
-</html>
-  `;
-
-  return (
-    <div dangerouslySetInnerHTML={{ __html: htmlContent }} />
-  );
-};
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <iframe src={`${process.env.PUBLIC_URL}/index.html`} title="External Content" width="100%" height="600px"></iframe>
+      </div>
+    );
+  }
+}
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,8 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import App from './App'; // Assuming App.js is in the same directory as index.js
+import { createRoot } from 'react-dom/client';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+
+const container = document.getElementById('app');
+const root = createRoot(container); // createRoot(container!) if you use TypeScript
+root.render(<App/>);
